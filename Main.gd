b7a07c005032bf50ae5ce1c302169a8d1f7b76ef
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-26 09:28:53 +0000

    27

--- Main.gd
+++ Main.gd
@@ -5,4 +5,4 @@ func _ready():
     	label.text = "Main OK"
         	label.add_theme_font_size_override("font_size", 64)
             	label.add_theme_color_override("font_color", Color.WHITE)
-                	add_child(label)extends
+                	add_child(label)extendsextends
