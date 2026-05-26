30c2f96b95545915f20aca632aad1541d384b4db
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-27 00:02:24 +0200

    update scripts/Kernel.gd

--- scripts/Kernel.gd
+++ scripts/Kernel.gd
@@ -8,10 +8,11 @@ var data: Node
 var states: Node
 var events: Node
 var utils: Node
+var builder: Node
+var skill_system: Node
 
 func _ready() -> void:
 	# Initialisiere alle Services als Kinder des Kernels
-	# Dadurch sind sie immer erreichbar unter: Kernel.world_factory...
 	events = _add_service("res://scripts/services/GameEvents.gd", "Events")
 	data = _add_service("res://scripts/services/DataService.gd", "Data")
 	states = _add_service("res://scripts/services/StateService.gd", "States")
@@ -19,6 +20,10 @@ func _ready() -> void:
 	
 	world_factory = _add_service("res://scripts/factories/WorldFactory.gd", "WorldFactory")
 	ui_factory = _add_service("res://scripts/factories/UIFactory.gd", "UIFactory")
+	
+	# Jetzt sind sie innerhalb der Funktion korrekt zugewiesen
+	builder = _add_service("res://scripts/services/InteractionBuilder.gd", "Builder")
+	skill_system = _add_service("res://scripts/services/SkillSystem.gd", "SkillSystem")
 
 func _add_service(path: String, node_name: String) -> Node:
 	var s = load(path).new()
