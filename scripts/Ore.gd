5fbb6ec1263d51b6659e0a102c06d3ba50359097
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-27 00:55:39 +0200

    update scripts/Ore.gd

--- scripts/Ore.gd
+++ scripts/Ore.gd
@@ -1,15 +1,16 @@
 extends Node3D
 
 func _ready():
+    # Visuelles (Einfacher grauer Block)
     var m = MeshInstance3D.new()
-        m.mesh = BoxMesh.new()
-            add_child(m)
+    m.mesh = BoxMesh.new()
+    add_child(m)
 
-                # KORREKTUR: Zugriff über Kernel.builder und Kernel.events
-                    Kernel.builder.create(self)\
-                            .set_label("Eisenerz abbauen")\
-                                    .set_duration(4.0)\
-                                            .on_complete(func(): 
-                                                        Kernel.events.emit_xp("mining", 40)
-                                                                    queue_free()
-                                                                            ).build()extends
+    # Interaktion 'bestellen' via Kernel
+    Kernel.builder.create(self)\
+        .set_label("Eisenerz abbauen")\
+        .set_duration(4.0)\
+        .on_complete(func(): 
+            Kernel.events.emit_xp("mining", 40)
+            queue_free()
+        ).build()
