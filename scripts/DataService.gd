fbb31559f5e11a74e420ee07ee2f6158ff8af75e
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-26 23:01:18 +0200

    update scripts/DataService.gd

---
+++ scripts/DataService.gd
@@ -0,0 +1,12 @@
+extends Node
+
+const TREES = {
+	"oak": {"xp": 50, "time": 5.0, "drop": "oak_log"},
+	"birch": {"xp": 20, "time": 2.5, "drop": "birch_log"}
+}
+
+const PLAYER_BASE_STATS = {
+	"speed": 6.0,
+	"jump_force": 4.5,
+	"interact_range": 4.0
+}
