33bfcfbcb7d285d5a1a7b1b2dc44fdad726af7ce
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-27 23:15:11 +0200

    update tests/IntegrationTest.gd

--- tests/IntegrationTest.gd
+++ tests/IntegrationTest.gd
@@ -10,6 +10,11 @@ func before_all():
     # Warte auf das neue Signal, das wir eingebaut haben
     await kernel.services_ready
 
+func before_each():
+    # Warte kurz, bis der Kernel fertig ist, falls er gerade erst bootet
+    if not Kernel._is_initialized:
+        await Kernel.services_ready
+        
 func after_all():
     if is_instance_valid(kernel):
         kernel.queue_free()
