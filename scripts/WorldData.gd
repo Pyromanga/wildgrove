679feae6143bf7bae375e1469dc1e87cebbcad6e
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-27 19:19:32 +0200

    update scripts/WorldData.gd

--- scripts/WorldData.gd
+++ scripts/WorldData.gd
@@ -6,3 +6,7 @@ var player_position: Vector3 = Vector3.ZERO
 
 func add_tree(pos: Vector3):
     tree_positions.append(pos)
+
+# NEU: Das ist die Logik-Prüfung für deine Tests!
+func get_tree_count() -> int:
+    return tree_positions.size()
