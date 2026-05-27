d5a69c07d6690e36c9e9cb55aa608a89f538122a
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-27 15:11:00 +0200

    update scripts/Ore.gd

--- scripts/Ore.gd
+++ scripts/Ore.gd
@@ -1,12 +1,10 @@
 extends InteractableObject
 
-func _ready() -> void:
-    # Variablen für das Erz setzen, bevor super()._ready() aufgerufen wird
+func _init() -> void:
     label = "Eisenerz abbauen"
     duration = 4.0
     xp_type = "mining"
     xp_amount = 40
-    super._ready() # Ruft das _ready() aus InteractableObject auf
 
 func _setup_visuals() -> void:
     var m := MeshInstance3D.new()
