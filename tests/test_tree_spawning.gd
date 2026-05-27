9a1cc27fe7e74f9770244930c1c3bed96980c3ab
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-27 17:03:12 +0200

    update tests/test_tree_spawning.gd

---
+++ tests/test_tree_spawning.gd
@@ -0,0 +1,7 @@
+func test_tree_spawning_at_correct_position():
+    var world = Kernel.world_factory.create_world()
+    var pos = Vector3(1, 1, 1)
+    Kernel.world_factory.create_tree(pos, world)
+    
+    var tree = world.get_node("Tree")
+    assert_eq(tree.position, pos, "Baum wurde an der falschen Position gespawnt")
