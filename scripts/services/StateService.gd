643e4db220f267414df579c81537c9b21ce58f19
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-27 14:21:45 +0200

    update scripts/services/StateService.gd

--- scripts/services/StateService.gd
+++ scripts/services/StateService.gd
@@ -6,8 +6,11 @@ var current_state: PlayerState = PlayerState.FREE
 
 func set_state(new_state: PlayerState) -> void:
 	current_state = new_state
-	# Zugriff nun über den Kernel, nicht mehr direkt über den Autoload-Namen
 	Kernel.events.log("Player Status: " + PlayerState.keys()[new_state])
+	Kernel.events.state_changed.emit(new_state)
 
 func is_free() -> bool:
 	return current_state == PlayerState.FREE
+
+func get_state() -> PlayerState:
+	return current_state
