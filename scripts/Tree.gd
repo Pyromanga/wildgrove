130c4681436ed3486f0481b32f45ff9247e23e3a
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-27 15:07:36 +0200

    update scripts/Tree.gd

--- scripts/Tree.gd
+++ scripts/Tree.gd
@@ -1,9 +1,13 @@
 extends InteractableObject
 
-func _setup_visuals() -> void:
-    var trunk = MeshInstance3D.new()
-    trunk.mesh = CylinderMesh.new()
-    add_child(trunk)
+func _init() -> void:
+	# Werte setzen, bevor _ready() läuft
+	label = "Eiche fällen"
+	duration = 3.5
+	xp_type = "woodcutting"
+	xp_amount = 25
 
-# _ready() entfällt hier komplett, da es in InteractableObject 
-# definiert ist und dort den Builder aufruft!
+func _setup_visuals() -> void:
+	var trunk = MeshInstance3D.new()
+	trunk.mesh = CylinderMesh.new()
+	add_child(trunk)
