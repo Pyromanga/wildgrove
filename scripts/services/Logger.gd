60cfbb221a7af0f9228cd049b8d13a2eb2fbeeae
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-30 00:10:59 +0200

    update scripts/services/Logger.gd

--- scripts/services/Logger.gd
+++ scripts/services/Logger.gd
@@ -1,10 +1,13 @@
 extends Node
+class_name Logger
 
-# Logging direkt beim Laden der Engine
+signal on_log(msg: String)
+
 func _init() -> void:
-	print("[Logger] Initialisiert (Pre-Ready)")
+    print("[Logger] Initialisiert (Pre-Ready)")
 
 func log_debug(msg: String, context: String = "System") -> void:
-	# Hier nutzen wir OS.get_ticks_msec() für echtes Timing
-	var time = Time.get_ticks_msec()
-	print("[%d] [%s] %s" % [time, context, msg])
+    var time := Time.get_ticks_msec()
+    var line := "[%d] [%s] %s" % [time, context, msg]
+    print(line)
+    on_log.emit(line)
