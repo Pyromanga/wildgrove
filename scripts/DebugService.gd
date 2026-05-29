0b7d30bb91cff6e9cd9f17132b0cc5ca8a8a8083
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-30 00:19:46 +0200

    Delete the file at 'scripts/DebugService.gd'

--- scripts/DebugService.gd
+++
@@ -1,8 +0,0 @@
-extends ServiceBase
-class_name DebugService
-
-func _ready() -> void:
-    # ServiceBase macht die Arbeit: Registrierung + Logging
-    super._ready()
-    # Der Test-Ping
-    Logger.log_debug("Ping-Pong-Service: 'Hallo, ich bin bereit!'", "DebugService")
