f26ce8b0c8f77a6a51294039a2b875ff476f9773
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-30 00:47:54 +0200

    Delete the file at 'scripts/World.gd'

--- scripts/World.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-## World.gd — Controller für Welt-Logik (z.B. Wetter, Tageszeit-Wechsel)
-
-func _ready() -> void:
-	# Falls du Welt-Logik brauchst, die erst läuft, 
-	# wenn die Welt komplett im Baum ist, kommt sie hier rein.
-	pass
