bc2f42c66003b9f7093b271c45115890df5249b1
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-31 13:27:51 +0200

    update scripts/services/DebugService.gd

--- scripts/services/DebugService.gd
+++ scripts/services/DebugService.gd
@@ -2,7 +2,7 @@ extends ServiceBase
 class_name DebugService
 
 func _ready() -> void:
-    # ServiceBase macht die Arbeit: Registrierung + Logging
-    super._ready()
-    # Der Test-Ping
-    Logger.log_debug("Ping-Pong-Service: 'Hallo, ich bin bereit!'", "DebugService")
+    if Kernel.has_method("register_service"):
+        Kernel.register_service("debug_service", self)
+        
+    Logger.log_debug("DebugService: 'Hallo, ich bin bereit!'", "DebugService")
