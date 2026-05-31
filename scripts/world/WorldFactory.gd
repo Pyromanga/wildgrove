f7e7c2d87b35523560760f94ea3c91a04c80aef7
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-31 13:08:03 +0200

    update scripts/world/WorldFactory.gd

--- scripts/world/WorldFactory.gd
+++ scripts/world/WorldFactory.gd
@@ -1,31 +1,39 @@
-# WorldFactory.gd
-func _add_player(world: Node3D, pos: Vector3) -> void:
-    Logger.log_debug("Lade Player-Script...", "WorldFactory")
-    var PlayerScript = load("res://scripts/player/Player.gd")
-    if not PlayerScript:
-        Logger.log_error("Player.gd konnte nicht geladen werden!", "WorldFactory")
-        return
+extends RefCounted
+class_name WorldFactory # <--- DAS FEHLTE!
 
-    # Sicherer Weg: Instanziiere direkt vom Script
-    var player = CharacterBody3D.new()
-    player.set_script(PlayerScript) 
-    player.name = "Player"
-    player.add_to_group("player") # WICHTIG für das HUD später
-    player.position = pos
-    world.add_child(player)
-    Logger.log_debug("Player OK und im Tree.", "WorldFactory")
+func create_world() -> Node3D:
+	Logger.log_debug("create_world() start", "WorldFactory")
+	var world := Node3D.new()
+	world.name = "World"
+	
+	# Hier rufen wir deine Hilfsmethoden auf
+	_add_environment(world)
+	_add_ground(world)
+	_add_player(world, Vector3(0, 1, 0))
+	_add_trees(world, [Vector3(5, 0, 5), Vector3(-6, 0, 4)])
+	
+	Logger.log_debug("create_world() fertig", "WorldFactory")
+	return world
+
+func _add_player(world: Node3D, pos: Vector3) -> void:
+	var PlayerScript = load("res://scripts/player/Player.gd")
+	if not PlayerScript:
+		Logger.log_error("Player.gd fehlt!", "WorldFactory")
+		return
+
+	var player := CharacterBody3D.new()
+	player.set_script(PlayerScript) 
+	player.name = "Player"
+	player.position = pos
+	player.add_to_group("player") 
+	world.add_child(player)
+	Logger.log_debug("Player OK.", "WorldFactory")
 
 func _add_trees(world: Node3D, positions: Array) -> void:
-    var TreeScript = load("res://scripts/world/objects/OakTree.gd")
-    if not TreeScript:
-        Logger.log_error("OakTree.gd fehlt!", "WorldFactory")
-        return
-
-    for i in positions.size():
-        var tree = Node3D.new()
-        tree.set_script(TreeScript)
-        tree.name = "OakTree_" + str(i)
-        tree.position = positions[i]
-        world.add_child(tree)
-        tree.add_to_group("interactable")
-        Logger.log_debug("Baum " + str(i) + " platziert.", "WorldFactory")
+	var TreeScript = load("res://scripts/world/objects/OakTree.gd")
+	for i in positions.size():
+		var tree := Node3D.new()
+		tree.set_script(TreeScript)
+		tree.position = positions[i]
+		world.add_child(tree)
+		tree.add_to_group("interactable")
