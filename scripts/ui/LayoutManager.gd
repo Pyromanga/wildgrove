816c6c77652b954aaf2af5b91d630d14fc4ca820
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-01 01:14:02 +0200

    update scripts/ui/LayoutManager.gd

---
+++ scripts/ui/LayoutManager.gd
@@ -0,0 +1,12 @@
+extends Node
+class_name LayoutManager
+
+func get_button_position(index: int) -> Vector2:
+    # Berechnet dynamisch die Position der Buttons
+    var margin = 30.0
+    var size = 150.0
+    return Vector2(...) # Hier die Mathematik
+
+func get_inventory_rect() -> Rect2:
+    # Berechnet, wo das Inventar liegen soll
+    return Rect2(...)
