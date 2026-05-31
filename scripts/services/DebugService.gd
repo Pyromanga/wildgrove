731edff39eb96503a0e268aedd5e934d8d0c78e1
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-05-31 13:32:31 +0200

    update scripts/services/DebugService.gd

--- scripts/services/DebugService.gd
+++ scripts/services/DebugService.gd
@@ -3,6 +3,5 @@ class_name DebugService
 
 func _ready() -> void:
     if Kernel.has_method("register_service"):
-        Kernel.register_service("debug_service", self)
-        
-    Logger.log_debug("DebugService: 'Hallo, ich bin bereit!'", "DebugService")
+        Kernel.register_service(self)
+    Logger.log_debug("DebugService bereit.", "DebugService")
