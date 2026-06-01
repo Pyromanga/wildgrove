71d87b69ef578776505bfb98038f1add9255fa53
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-01 22:02:56 +0200

    update scripts/resources/PlayerData.gd

---
+++ scripts/resources/PlayerData.gd
@@ -0,0 +1,7 @@
+# res://scripts/resources/PlayerData.gd
+class_name PlayerData extends Resource
+
+@export var speed: float = 6.0
+@export var gravity: float = 12.0
+@export var jump_force: float = 4.5
+@export var interact_range: float = 4.0
