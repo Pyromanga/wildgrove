9bb8aaf3ed861d8909220d36d8b54b5b1326a2b8
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-01 20:26:48 +0200

    update scripts/core/ServiceDefinition.gd

---
+++ scripts/core/ServiceDefinition.gd
@@ -0,0 +1,11 @@
+# res://scripts/core/ServiceDefinition.gd
+class_name ServiceDefinition extends Resource
+
+@export var name: String
+@export var path: String
+@export var deps: Array[String]
+
+# res://scripts/core/BootstrapConfig.gd
+class_name BootstrapConfig extends Resource
+
+@export var services: Array[ServiceDefinition]
