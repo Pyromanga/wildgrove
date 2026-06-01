4af481fb712a4f514e292e99ba37676ef5b04895
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-01 17:46:07 +0200

    update scripts/interaction/InteractionSensor.gd

---
+++ scripts/interaction/InteractionSensor.gd
@@ -0,0 +1,14 @@
+# scripts/player/interaction_sensor.gd
+class_name InteractionSensor extends Area3D
+
+func get_closest() -> Node:
+    var bodies = get_overlapping_bodies()
+    var closest = null
+    var min_dist = 999.0
+    for b in bodies:
+        if b.is_in_group("interactable"):
+            var dist = global_position.distance_to(b.global_position)
+            if dist < min_dist:
+                min_dist = dist
+                closest = b
+    return closest
