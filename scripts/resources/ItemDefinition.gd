6fd75c4aa6a8cccf8efcbfa1c10f8c72030a6071
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-01 22:18:18 +0200

    update scripts/resources/ItemDefinition.gd

---
+++ scripts/resources/ItemDefinition.gd
@@ -0,0 +1,7 @@
+# res://scripts/resources/ItemDefinition.gd
+class_name ItemDefinition extends Resource
+
+@export var id: String = ""
+@export var display_name: String = ""
+@export var icon: Texture2D
+@export var max_stack: int = 99
