857020cc0f9f294db742512d3d53d9286c41fe2b
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-01 18:18:59 +0200

    update scripts/ui/components/JoystickComponent.gd

--- scripts/ui/components/JoystickComponent.gd
+++ scripts/ui/components/JoystickComponent.gd
@@ -1,7 +1,11 @@
+# scripts/ui/components/joystick_component.gd
 class_name JoystickComponent extends BaseUIComponent
 
 func build(hud: HUD) -> JoystickController:
     var visuals = JoystickVisuals.new(hud)
     var ctrl = JoystickController.new()
-    ctrl.setup(visuals)
+    
+    # Injection passiert hier:
+    ctrl.setup(visuals, Kernel.events.ui)
+    
     return ctrl
