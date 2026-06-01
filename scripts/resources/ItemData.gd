9f7f8f86605473035089162af7958c1daafdf80d
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-01 22:20:48 +0200

    Delete the file at 'scripts/resources/ItemData.gd'

--- scripts/resources/ItemData.gd
+++
@@ -1,7 +0,0 @@
-# res://scripts/resources/ItemData.gd
-class_name ItemData extends Resource
-
-@export var id: String = ""
-@export var display_name: String = ""
-@export var icon: Texture2D
-@export var stackable: bool = true
