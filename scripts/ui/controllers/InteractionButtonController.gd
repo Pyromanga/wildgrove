84fb9efd03ac9ddc16b5a720a7911305d27efc70
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-01 11:43:19 +0200

    update scripts/ui/controllers/InteractionButtonController.gd

--- scripts/ui/controllers/InteractionButtonController.gd
+++ scripts/ui/controllers/InteractionButtonController.gd
@@ -6,7 +6,7 @@ var _player: Node
 
 func setup(visuals: ButtonVisuals) -> void:
     _visuals = visuals
-    # Suche den Spieler im Baum (oder über den Service-Locator)
+    # Spieler suchen (oder über Kernel)
     var players = Engine.get_main_loop().root.get_nodes_in_group("player")
     if not players.is_empty():
         _player = players[0]
@@ -18,5 +18,4 @@ func _process(_delta: float) -> void:
     if _player.has_method("_get_closest_interactable"):
         target = _player._get_closest_interactable()
         
-    # Wir sagen den Visuals nur: "Ändere den Status"
     _visuals.set_active(target != null)
