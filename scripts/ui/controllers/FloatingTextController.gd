9fbb2aad2a71c268639efea5ab3dfdef90223256
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-01 15:50:50 +0200

    update scripts/ui/controllers/FloatingTextController.gd

--- scripts/ui/controllers/FloatingTextController.gd
+++ scripts/ui/controllers/FloatingTextController.gd
@@ -1,14 +1,11 @@
 class_name FloatingTextController
 
-var _hud: HUD
+var _visuals: FloatingTextVisuals
 
-func setup(hud: HUD) -> void:
-    _hud = hud
+func setup(visuals: FloatingTextVisuals) -> void:
+    _visuals = visuals
     Kernel.events.player.xp_gained.connect(_on_xp_gained)
     Kernel.events.skill_system.level_up.connect(_on_level_up)
 
 func _on_xp_gained(skill: String, amount: int) -> void:
-    _hud.show_floating_text("+%d %s" % [amount, skill], Color(1, 0.9, 0.2))
-
-func _on_level_up(skill: String, new_level: int) -> void:
-    _hud.show_floating_text("%s Stufe %d!" % [skill, new_level], Color(0.2, 1, 0.2), 36, 3.0)
+    _visuals.spawn_text("+%d %s" % [amount, skill], Color(1, 0.9, 0.2), 24, 2.0)
