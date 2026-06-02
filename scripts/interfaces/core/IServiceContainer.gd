039478dbf8fcf5acf2cb944518b9d92ff1b9d96f
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-02 21:00:32 +0200

    update scripts/interfaces/core/IServiceContainer.gd

---
+++ scripts/interfaces/core/IServiceContainer.gd
@@ -0,0 +1,10 @@
+# res://scripts/interfaces/core/IServiceContainer.gd
+class_name IServiceContainer extends RefCounted
+
+var events: Object
+var states: Object
+var builder: Object
+var inventory: Object
+var data: Object
+var factory3d: Object
+var world: Object
