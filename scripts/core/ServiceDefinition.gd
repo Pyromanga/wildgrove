86cfa8488c097843e22cd06fa9b3b7157649de15
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-02 23:21:12 +0200

    update scripts/core/ServiceDefinition.gd

--- scripts/core/ServiceDefinition.gd
+++ scripts/core/ServiceDefinition.gd
@@ -3,4 +3,3 @@ class_name ServiceDefinition extends Resource
 @export var service_name: String = ""
 @export_file("*.gd", "*.tscn") var path: String = ""
 @export var deps: Array[String] = []
-@export var is_node_service: bool = true
