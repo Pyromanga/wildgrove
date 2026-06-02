9c37b2e4a5ec5805b4818c46035fa8f091bcdb7f
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-02 21:27:28 +0200

    update scripts/interfaces/core/IKernel.gd

--- scripts/interfaces/core/IKernel.gd
+++ scripts/interfaces/core/IKernel.gd
@@ -5,14 +5,9 @@ class_name IKernel extends Node
 signal service_registered(service_name: String)
 signal service_unregistered(service_name: String)
 
-var shortcuts: Object 
-
 ## Registry API
 func register_service(_service: Object) -> void: pass
 func unregister_service(_service: Object) -> void: pass
 func get_service(_service_name: String) -> Object: return null
 func has_service(_service_name: String) -> bool: return false
 func get_registered_names() -> Array[String]: return []
-
-## Lifecycle
-func bind_shortcuts() -> void: pass
