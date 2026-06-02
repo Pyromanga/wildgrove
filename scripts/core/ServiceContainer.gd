36297ecddf3f06132d56ec92843569283f33177f
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-02 21:19:53 +0200

    update scripts/core/ServiceContainer.gd

--- scripts/core/ServiceContainer.gd
+++ scripts/core/ServiceContainer.gd
@@ -2,10 +2,10 @@
 class_name ServiceContainer extends RefCounted
 
 ## Hier darfst du deine Klassen typisieren!
-var events: GameEvents
-var states: PlayerStates
-var builder: BuilderService
-var inventory: InventoryService
+var game_events: GameEvents
+var player_states: PlayerStates
+var interaction_builder: BuilderService
+var inventory_sytem: InventoryService
 var data: DataService
 var factory3d: Factory3DService
 var world: WorldService
