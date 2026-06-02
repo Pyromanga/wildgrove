222544a4c922e40a00436635378c48a10daf1a56
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-02 16:30:51 +0200

    Delete the file at 'data/items/LogMaple.gd'

--- data/items/LogMaple.gd
+++
@@ -1,8 +0,0 @@
-[gd_resource type="Resource" script_class="ItemDefinition" format=3]
-[ext_resource type="Script" path="res://scripts/resources/ItemDefinition.gd" id="1_def"]
-
-[resource]
-script = ExtResource("1_def")
-id = "log_maple"
-display_name = "Ahornholz"
-max_stack = 99
