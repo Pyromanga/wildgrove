9f087623a72e36484e7c99c9e19db2e333096fed
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-04 00:35:49 +0200

    update scripts/core/helpers/StateValidator.gd

--- scripts/core/helpers/StateValidator.gd
+++ scripts/core/helpers/StateValidator.gd
@@ -1,15 +1,18 @@
 class_name StateValidator
 
-static func is_transition_allowed(from: GameEnums.State, to: GameEnums.State, config: GameConfig) -> bool:
-    if not config:
-        return true # Oder Fallback-Logik
-        
-    var from_str := GameEnums.State.keys()[from]
-    var to_str   := GameEnums.State.keys()[to]
-    var allowed: Array = config.valid_transitions.get(from_str, [])
-    
-    if not to_str in allowed:
-        Logger.log_warn("Ungültiger Übergang: %s -> %s" % [from_str, to_str], "StateValidator")
-        return false
-        
-    return true
+
+static func is_transition_allowed(
+	from: GameEnums.State, to: GameEnums.State, config: GameConfig
+) -> bool:
+	if not config:
+		return true  # Oder Fallback-Logik
+
+	var from_str: String = GameEnums.State.keys()[from]
+	var to_str: String = GameEnums.State.keys()[to]
+	var allowed: Array = config.valid_transitions.get(from_str, [])
+
+	if not to_str in allowed:
+		Logger.log_warn("Ungültiger Übergang: %s -> %s" % [from_str, to_str], "StateValidator")
+		return false
+
+	return true
