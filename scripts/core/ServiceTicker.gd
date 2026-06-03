c62860c69b122befe5e1b1cde7f2a9860d2e6f5d
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-03 21:04:53 +0200

    update scripts/core/ServiceTicker.gd

--- scripts/core/ServiceTicker.gd
+++ scripts/core/ServiceTicker.gd
@@ -1,4 +1,3 @@
-# res://scripts/core/ServiceTicker.gd
 extends Node
 class_name ServiceTicker
 
@@ -7,25 +6,33 @@ const LOG_CAT := "ServiceTicker"
 var _tick_list: Array[Object] = []
 var _physics_tick_list: Array[Object] = []
 
-## Registriert einen Service für Updates
 func register_service(svc: Object) -> void:
 	var registered := false
-	
-	if svc.has_method("on_tick"):
+	# Doppelte Einträge verhindern mit 'not in'
+	if svc.has_method("on_tick") and not svc in _tick_list:
 		_tick_list.append(svc)
 		registered = true
 		
-	if svc.has_method("on_physics_tick"):
+	if svc.has_method("on_physics_tick") and not svc in _physics_tick_list:
 		_physics_tick_list.append(svc)
 		registered = true
 	
 	if registered:
-		Logger.log_debug("Service '%s' für Ticks registriert." % svc.get_class(), LOG_CAT)
+		Logger.log_debug("Service '%s' registriert." % svc.get_class(), LOG_CAT)
 
 func _process(delta: float) -> void:
-	for svc in _tick_list:
-		svc.on_tick(delta)
+	# Rückwärts laufen ist ein Standard-Pattern: Sicher gegen Löschen während des Loops
+	for i in range(_tick_list.size() - 1, -1, -1):
+		var svc = _tick_list[i]
+		if is_instance_valid(svc):
+			svc.on_tick(delta)
+		else:
+			_tick_list.remove_at(i)
 
 func _physics_process(delta: float) -> void:
-	for svc in _physics_tick_list:
-		svc.on_physics_tick(delta)
+	for i in range(_physics_tick_list.size() - 1, -1, -1):
+		var svc = _physics_tick_list[i]
+		if is_instance_valid(svc):
+			svc.on_physics_tick(delta)
+		else:
+			_physics_tick_list.remove_at(i)
