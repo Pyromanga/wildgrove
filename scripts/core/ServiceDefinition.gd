ff0541fd58431ed2a4ec93155c3f9721be0fc2a8
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-03 14:38:54 +0200

    update scripts/core/ServiceDefinition.gd

--- scripts/core/ServiceDefinition.gd
+++ scripts/core/ServiceDefinition.gd
@@ -3,3 +3,5 @@ class_name ServiceDefinition extends Resource
 @export var service_name: String = ""
 @export_file("*.gd", "*.tscn") var path: String = ""
 @export var deps: Array[String] = []
+@export_file("*.tres", "*.res") var required_data_files: Array[String] = []
+@export var interface_type: String = ""
