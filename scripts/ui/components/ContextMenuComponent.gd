559b2402d5592ea9fbc70bf04688085dbfc0cd59
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 00:50:53 +0200

    update scripts/ui/components/ContextMenuComponent.gd

--- scripts/ui/components/ContextMenuComponent.gd
+++ scripts/ui/components/ContextMenuComponent.gd
@@ -1,5 +1,6 @@
 class_name ContextMenuComponent extends BaseUIComponent
 
+
 # FIX: Kein Override-Konflikt mehr, da BaseUIComponent kein build() hat.
 func build(hud: HUD, event_bus: Object) -> ContextMenuController:
 	var ctrl := ContextMenuController.new()
