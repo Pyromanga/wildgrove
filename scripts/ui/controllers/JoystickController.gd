444bd32f46ac7ff517386fa19584d743811717f9
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 00:55:49 +0200

    update scripts/ui/controllers/JoystickController.gd

--- scripts/ui/controllers/JoystickController.gd
+++ scripts/ui/controllers/JoystickController.gd
@@ -3,20 +3,24 @@ class_name JoystickController
 
 var _visuals: JoystickVisuals
 
+
 # Injection: Wir übergeben den Event-Bus explizit
 func setup(visuals: JoystickVisuals, event_bus: Object) -> void:
-    _visuals = visuals
-    event_bus.joystick_toggled.connect(_on_toggled)
-    event_bus.joystick_moved.connect(_on_moved)
+	_visuals = visuals
+	event_bus.joystick_toggled.connect(_on_toggled)
+	event_bus.joystick_moved.connect(_on_moved)
+
 
 # ... der Rest des Codes bleibt identisch ...
 
+
 func _on_toggled(active: bool, origin: Vector2) -> void:
-    _visuals.set_visible(active)
-    if active:
-        # Hier fragst du später den LayoutManager!
-        _visuals.base.global_position = origin - (_visuals.base.size / 2)
+	_visuals.set_visible(active)
+	if active:
+		# Hier fragst du später den LayoutManager!
+		_visuals.base.global_position = origin - (_visuals.base.size / 2)
+
 
 func _on_moved(origin: Vector2, offset: Vector2) -> void:
-    # Nur Logik-Update, Visuals werden verschoben
-    _visuals.knob.global_position = origin + offset - (_visuals.knob.size * 0.5)
+	# Nur Logik-Update, Visuals werden verschoben
+	_visuals.knob.global_position = origin + offset - (_visuals.knob.size * 0.5)
