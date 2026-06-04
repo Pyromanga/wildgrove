b8064f53e957c1b3b9258a6e786fad8cf77d9cc0
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 00:36:45 +0200

    update scripts/interaction/InteractableData.gd

--- scripts/interaction/InteractableData.gd
+++ scripts/interaction/InteractableData.gd
@@ -7,5 +7,5 @@ class_name InteractableData
 @export var duration: float = 1.5
 @export var xp_type: String = "none"
 @export var xp_amount: int = 10
-@export var drops: Dictionary = {} # { "item_id": quantity }
+@export var drops: Dictionary = {}  # { "item_id": quantity }
 @export var inspect_text: String = ""
