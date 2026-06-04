7707b9ef5567200e7126c1b36148f9edcabcd5da
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 00:53:32 +0200

    update scripts/ui/controllers/ContextButtonController.gd

--- scripts/ui/controllers/ContextButtonController.gd
+++ scripts/ui/controllers/ContextButtonController.gd
@@ -3,11 +3,13 @@ class_name ContextButtonController
 var _visuals: ContextButtonVisuals
 var _event_bus: Object
 
+
 func setup(visuals: ContextButtonVisuals, event_bus: Object) -> void:
-    _visuals = visuals
-    _event_bus = event_bus
-    _visuals.button.pressed.connect(_on_pressed)
+	_visuals = visuals
+	_event_bus = event_bus
+	_visuals.button.pressed.connect(_on_pressed)
+
 
 func _on_pressed() -> void:
-    # Nur noch ein Event feuern!
-    _event_bus.request_context_menu.emit()
+	# Nur noch ein Event feuern!
+	_event_bus.request_context_menu.emit()
