1cb73023329204242bb1d3636609105ddb00a63c
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 00:53:09 +0200

    update scripts/ui/components/NotificationComponent.gd

--- scripts/ui/components/NotificationComponent.gd
+++ scripts/ui/components/NotificationComponent.gd
@@ -1,7 +1,8 @@
 class_name NotificationComponent extends BaseUIComponent
 
+
 func build(hud: HUD) -> NotificationController:
 	var visuals := NotificationVisuals.new(hud)
-	var ctrl    := NotificationController.new()
+	var ctrl := NotificationController.new()
 	ctrl.setup(visuals)
 	return ctrl
