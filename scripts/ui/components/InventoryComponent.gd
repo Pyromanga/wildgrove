4997f5c29f23338422737863882a1d9b8a1158aa
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 00:52:34 +0200

    update scripts/ui/components/InventoryComponent.gd

--- scripts/ui/components/InventoryComponent.gd
+++ scripts/ui/components/InventoryComponent.gd
@@ -1,8 +1,9 @@
 class_name InventoryComponent extends BaseUIComponent
 
+
 # FIX: Kein Override-Konflikt mehr, da BaseUIComponent kein build() hat.
 func build(hud: HUD, inv_service: InventorySystem) -> InventoryUIController:
 	var visuals := InventoryVisuals.new(hud)
-	var ctrl    := InventoryUIController.new()
+	var ctrl := InventoryUIController.new()
 	ctrl.setup(visuals, inv_service)
 	return ctrl
