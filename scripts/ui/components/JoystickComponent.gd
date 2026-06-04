98d4ea0e15f871bce6850cfc2d9f10b6170c1883
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 00:52:54 +0200

    update scripts/ui/components/JoystickComponent.gd

--- scripts/ui/components/JoystickComponent.gd
+++ scripts/ui/components/JoystickComponent.gd
@@ -1,7 +1,8 @@
 class_name JoystickComponent extends BaseUIComponent
 
+
 func build(hud: HUD) -> JoystickController:
 	var visuals := JoystickVisuals.new(hud)
-	var ctrl    := JoystickController.new()
+	var ctrl := JoystickController.new()
 	ctrl.setup(visuals, EventBus.ui)
 	return ctrl
