1c7fd3a935f8e7db17f19bd541b4b67069d1749a
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 00:54:31 +0200

    update scripts/ui/controllers/InteractionUIController.gd

--- scripts/ui/controllers/InteractionUIController.gd
+++ scripts/ui/controllers/InteractionUIController.gd
@@ -2,21 +2,25 @@ class_name InteractionUIController
 
 var _visuals: InteractionVisuals
 
+
 func setup(visuals: InteractionVisuals, world_events: Object) -> void:
-    _visuals = visuals
-    world_events.interaction_started.connect(_on_started)
-    world_events.interaction_finished.connect(_on_finished)
-    world_events.interaction_cancelled.connect(_on_cancelled)
+	_visuals = visuals
+	world_events.interaction_started.connect(_on_started)
+	world_events.interaction_finished.connect(_on_finished)
+	world_events.interaction_cancelled.connect(_on_cancelled)
+
 
 func _on_started(_label: String, duration: float) -> void:
-    _visuals.set_value(0)
-    _visuals.set_visible(true)
-    
-    var tween = _visuals.create_tween()
-    tween.tween_property(_visuals.bar, "value", 100.0, duration)
+	_visuals.set_value(0)
+	_visuals.set_visible(true)
+
+	var tween = _visuals.create_tween()
+	tween.tween_property(_visuals.bar, "value", 100.0, duration)
+
 
 func _on_finished(_label: String) -> void:
-    _visuals.set_visible(false)
+	_visuals.set_visible(false)
+
 
 func _on_cancelled(_label: String) -> void:
-    _visuals.set_visible(false)
+	_visuals.set_visible(false)
