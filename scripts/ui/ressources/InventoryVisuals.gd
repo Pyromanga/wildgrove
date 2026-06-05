01221366782c7f51bf8a416f998efed0c5721318
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 10:32:35 +0200

    update scripts/ui/ressources/InventoryVisuals.gd

--- scripts/ui/ressources/InventoryVisuals.gd
+++ scripts/ui/ressources/InventoryVisuals.gd
@@ -3,17 +3,20 @@ class_name InventoryVisuals
 var panel: PanelContainer
 var label: Label
 
+
 func _init(parent: CanvasLayer) -> void:
-    panel = PanelContainer.new()
-    panel.visible = false
-    panel.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
-    
-    label = Label.new()
-    panel.add_child(label)
-    parent.add_child(panel)
+	panel = PanelContainer.new()
+	panel.visible = false
+	panel.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+
+	label = Label.new()
+	panel.add_child(label)
+	parent.add_child(panel)
+
 
 func update_text(text: String) -> void:
-    label.text = text
+	label.text = text
+
 
 func toggle() -> void:
-    panel.visible = !panel.visible
+	panel.visible = !panel.visible
