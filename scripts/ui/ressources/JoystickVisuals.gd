4d626b2397b32c53f1d13dfe845dab1eeac733b1
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 10:32:53 +0200

    update scripts/ui/ressources/JoystickVisuals.gd

--- scripts/ui/ressources/JoystickVisuals.gd
+++ scripts/ui/ressources/JoystickVisuals.gd
@@ -3,17 +3,19 @@ class_name JoystickVisuals
 var base: ColorRect
 var knob: ColorRect
 
+
 func _init(parent: CanvasLayer) -> void:
-    base = ColorRect.new()
-    base.custom_minimum_size = Vector2(180, 180)
-    base.visible = false
-    parent.add_child(base)
-    
-    knob = ColorRect.new()
-    knob.custom_minimum_size = Vector2(60, 60)
-    knob.visible = false
-    parent.add_child(knob)
+	base = ColorRect.new()
+	base.custom_minimum_size = Vector2(180, 180)
+	base.visible = false
+	parent.add_child(base)
+
+	knob = ColorRect.new()
+	knob.custom_minimum_size = Vector2(60, 60)
+	knob.visible = false
+	parent.add_child(knob)
+
 
 func set_visible(val: bool) -> void:
-    base.visible = val
-    knob.visible = val
+	base.visible = val
+	knob.visible = val
