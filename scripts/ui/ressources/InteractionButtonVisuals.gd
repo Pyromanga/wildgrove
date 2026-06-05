0a1e733c19016bf0133298849bdbdaaf0e4c2475
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 11:23:16 +0200

    update scripts/ui/ressources/InteractionButtonVisuals.gd

--- scripts/ui/ressources/InteractionButtonVisuals.gd
+++ scripts/ui/ressources/InteractionButtonVisuals.gd
@@ -8,5 +8,9 @@ func _init(parent: CanvasLayer, pos: Vector2) -> void:
 	button = Button.new()
 	button.text = "!"
 	button.custom_minimum_size = Vector2(150, 150)
-	button.position = pos  # Position wird von außen diktiert
+	button.position = pos
 	parent.add_child(button)
+
+
+func set_active(active: bool) -> void:
+	button.visible = active
