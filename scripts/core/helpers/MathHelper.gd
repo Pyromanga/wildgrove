a8cdc25c941a9bac2419b264642afa4c6d9f3cbb
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2026-06-05 10:33:57 +0200

    update scripts/core/helpers/MathHelper.gd

--- scripts/core/helpers/MathHelper.gd
+++ scripts/core/helpers/MathHelper.gd
@@ -1,12 +1,13 @@
 class_name MathHelper
 
+
 ## Kamera-relative Bewegungsrichtung aus Input berechnen.
 ## camera_arm: Node3D der als Kamera-Basis dient (SpringArm3D / PlayerCamera)
 ## input:      Joystick-Vektor (x = rechts, y = runter)
 static func calculate_move_direction(camera_arm: Node3D, input: Vector2) -> Vector3:
 	if not is_instance_valid(camera_arm):
 		return Vector3.ZERO
-	var basis   := camera_arm.global_transform.basis
+	var basis := camera_arm.global_transform.basis
 	var forward := Vector3(-basis.z.x, 0.0, -basis.z.z).normalized()
-	var right   := Vector3( basis.x.x, 0.0,  basis.x.z).normalized()
+	var right := Vector3(basis.x.x, 0.0, basis.x.z).normalized()
 	return (forward * -input.y + right * input.x).normalized()
